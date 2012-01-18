e7de03a93971dde88c41e4dc2729e778fe5f3633
Joe <cpmhjoe@gmail.com>
2012-01-17 21:16:27 -0500

    users finally completely finished

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ ApplicationManager::Application.routes.draw do
   resources :users
   resources :sessions, :only => [:new, :create, :destroy, :index]
   
-  root :to => 'sessions#new'
+  root :to => 'users#index'
   
   match '/signup',		:to => 'users#new'
   match '/signin',		:to	=> 'sessions#new'
